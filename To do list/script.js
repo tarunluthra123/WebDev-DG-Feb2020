dbd67b3a235d0b5ff2a5ab5a9776609ad99a6c0a
Tarun Luthra <tarunluthra987@gmail.com>
2021-02-15 15:56:55 +0530

    Lecture 22 - To do list complete

--- To do list/script.js
+++ To do list/script.js
@@ -6,19 +6,51 @@ const taskList = document.getElementById('taskList')
 addTaskBtn.onclick = function () {
     const newTask = inputBox.value
 
+    if (newTask.length == 0)
+        return
+
     inputBox.value = ''
 
     const listItem = document.createElement('li')
     const crossBtn = document.createElement('button')
+    const upBtn = document.createElement('button')
+    const downBtn = document.createElement('button')
+    const span = document.createElement('span')
+
+    listItem.classList.add('task')
+    crossBtn.classList.add('crossBtn')
+    upBtn.classList.add('upBtn')
+    downBtn.classList.add('downBtn')
 
     crossBtn.innerText = 'X'
+    upBtn.innerText = '↑'
+    downBtn.innerText = '↓'
 
     crossBtn.onclick = function () {
         listItem.remove()
     }
 
+    upBtn.onclick = function () {
+        taskList.insertBefore(
+            listItem,
+            listItem.previousElementSibling
+        )
+    }
+
+    downBtn.onclick = function () {
+        taskList.insertBefore(
+            listItem,
+            listItem.nextElementSibling.nextElementSibling
+        )
+    }
+
     listItem.append(newTask)
-    listItem.append(crossBtn)
+
+    span.append(crossBtn)
+    span.append(upBtn)
+    span.append(downBtn)
+
+    listItem.append(span)
 
     taskList.append(listItem)
 }
